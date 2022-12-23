3ab108be470ac75641f3e4ca9cf9b446bf8f19ba
francoa7 <francoangulo2001@gmail.com>
2022-12-23 19:01:30 -0300

    Handling getOrder function

--- actions/getOrder.js
+++ actions/getOrder.js
@@ -3,7 +3,6 @@ const SQSHelper = require("../config/SQSHelper");
 const fs = require("fs");
 var chalk = require("chalk");
 
-console.log({ ENDPOINT: process.env.GET_ORDER });
 (async () => {
   const sqs = new SQSHelper();
   const params = {
@@ -11,8 +10,18 @@ console.log({ ENDPOINT: process.env.GET_ORDER });
     Action: "ReceiveMessage",
   };
   const result = await sqs.receiveMessage(params);
-  console.log(result);
-  fs.writeFile("pruebaGet.json", JSON.stringify(result), () => {
+  const orderNumber = result.Messages
+    ? getOrderNumber(result.Messages[0].Body)
+    : "no-order";
+  fs.writeFile(`${orderNumber}.json`, JSON.stringify(result), () => {
     console.log(chalk.bgGreen.bold("\n\n Exported file successfully!"));
   });
 })();
+
+const getOrderNumber = (body) => {
+  const start =
+    String(body).lastIndexOf("<OrderNumber>") + "<OrderNumber>".length;
+  const end = String(body).lastIndexOf("</OrderNumber>");
+  const newName = String(body).substring(start, end);
+  return newName;
+};
--- config/SQSHelper.js
+++ config/SQSHelper.js
@@ -1,30 +1,30 @@
 const {
-    SQSClient,
-    ListQueuesCommand,
-    GetQueueAttributesCommand,
-    SendMessageCommand,
-    ReceiveMessageCommand,
+  SQSClient,
+  ListQueuesCommand,
+  GetQueueAttributesCommand,
+  SendMessageCommand,
+  ReceiveMessageCommand,
 } = require("@aws-sdk/client-sqs");
 
 class SQSHelper {
-    constructor() {
-        this.client = new SQSClient();
-    }
+  constructor() {
+    this.client = new SQSClient();
+  }
 
-    listQueues() {
-        return this.client.send(new ListQueuesCommand({}));
-    }
+  listQueues() {
+    return this.client.send(new ListQueuesCommand({}));
+  }
 
-    getQueueAttributes(params) {
-        return this.client.send(new GetQueueAttributesCommand(params));
-    }
+  getQueueAttributes(params) {
+    return this.client.send(new GetQueueAttributesCommand(params));
+  }
 
-    sendMessage(params) {
-        return this.client.send(new SendMessageCommand(params));
-    }
-    receiveMessage(params) {
-        return this.client.send(new ReceiveMessageCommand(params));
-    }
+  sendMessage(params) {
+    return this.client.send(new SendMessageCommand(params));
+  }
+  receiveMessage(params) {
+    return this.client.send(new ReceiveMessageCommand(params));
+  }
 }
 
 module.exports = SQSHelper;
