4886a1c5054bed12f1a8bb694f13425e2e51820f
francoa7 <francoangulo2001@gmail.com>
2022-12-23 17:46:46 -0300

    Sending testings

--- actions/getOrder.js
+++ actions/getOrder.js
@@ -1,17 +1,18 @@
-require("dotenv").config();
+require("dotenv").config({ path: `.env.${process.env.ENVIRONMENT}` });
 const SQSHelper = require("../config/SQSHelper");
 const fs = require("fs");
 var chalk = require("chalk");
 
+console.log({ ENDPOINT: process.env.GET_ORDER });
 (async () => {
-    const sqs = new SQSHelper();
-    const params = {
-        QueueUrl:
-            "https://sqs.eu-west-1.amazonaws.com/765151898027/QAS_ArgentinaStore_55710?Action=ReceiveMessage&MaxNumberOfMessages=5&VisibilityTimeout=15&AttributeName=All",
-    };
-    const result = await sqs.receiveMessage(params);
-    console.log(result);
-    fs.writeFile("pruebaGet.txt", JSON.stringify(result), () => {
-        console.log(chalk.bgGreen.bold("\n\n Exported file successfully!"));
-    });
+  const sqs = new SQSHelper();
+  const params = {
+    QueueUrl: process.env.GET_ORDER,
+    Action: "ReceiveMessage",
+  };
+  const result = await sqs.receiveMessage(params);
+  console.log(result);
+  fs.writeFile("pruebaGet.json", JSON.stringify(result), () => {
+    console.log(chalk.bgGreen.bold("\n\n Exported file successfully!"));
+  });
 })();
--- actions/receiveMessage.js
+++ actions/receiveMessage.js
@@ -1,18 +1,17 @@
-require("dotenv").config();
+require("dotenv").config({ path: `.env.${process.env.ENVIRONMENT}` });
 const SQSHelper = require("../config/SQSHelper");
 const fs = require("fs");
 var chalk = require("chalk");
-
+console.log({ ENV: process.env.REGION });
 (async () => {
-    const sqs = new SQSHelper();
-    const params = {
-        QueueUrl:
-            "https://sqs.eu-west-1.amazonaws.com/765151898027/QAS_ArgentinaStore_55710_ResponsesFromSalesforce",
-        AttributeName: ["All"],
-    };
-    const result = await sqs.receiveMessage(params);
-    console.log(result);
-    fs.writeFile("prueba.txt", JSON.stringify(result), () => {
-        console.log(chalk.bgGreen.bold("\n\n Exported file successfully!"));
-    });
+  const sqs = new SQSHelper();
+  const params = {
+    QueueUrl: process.env.RECEIVE_MESSAGE,
+    AttributeName: ["All"],
+  };
+  const result = await sqs.receiveMessage(params);
+  console.log(result);
+  fs.writeFile("prueba.txt", JSON.stringify(result), () => {
+    console.log(chalk.bgGreen.bold("\n\n Exported file successfully!"));
+  });
 })();
