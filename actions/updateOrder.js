147c7bc4ca7a0c28861a647f15c5b84ee7037b29
francoa7 <francoangulo2001@gmail.com>
2022-12-24 17:05:58 -0300

    Updated message body for update action

--- actions/updateOrder.js
+++ actions/updateOrder.js
@@ -7,8 +7,9 @@ import chalk from "chalk";
 (async () => {
   const params = {
     QueueUrl: process.env.UPDATE_ORDER,
+    //     TODO: Change this messsage body with the encoded body set by the user (to be read from a file)
     MessageBody:
-      "%3C%3Fxml%20version%3D%271.0%27%20encoding%3D%27UTF-8%27%3F%3E%0A%3COrderUpdates%3E%0A%3COrderStatus%3E%0A%3CSellerId%3E50110%3C%2FSellerId%3E%20%0A%3CStorefront%3EArgentinaStore%3C%2FStorefront%3E%0A%3COrderNumber%3E5902149%3C%2FOrderNumber%3E%20%0A%3COrderStatus%3EDelivered%3C%2FOrderStatus%3E%20%0A%3CEZDOrderNumber%3E12345%3C%2FEZDOrderNumber%3E%20%0A%3C%2FOrderStatus%3E%0A%3C%2FOrderUpdates%3E%0A",
+      "%3C%3Fxml%20version%3D%271.0%27%20encoding%3D%27UTF-8%27%3F%3E%0A%3COrderUpdates%3E%0A%3COrderStatus%3E%0A%3CSellerId%3E55710%3C%2FSellerId%3E%20%0A%3CStorefront%3EArgentinaStore%3C%2FStorefront%3E%0A%3COrderNumber%3E5902149%3C%2FOrderNumber%3E%20%0A%3CStatus%3EDelivered%3C%2FStatus%3E%20%0A%3CEZDOrderNumber%3E12345%3C%2FEZDOrderNumber%3E%20%0A%3C%2FOrderStatus%3E%0A%3C%2FOrderUpdates%3E%0A",
   };
   try {
     console.log(await sqsClient.send(new SendMessageCommand(params)));
